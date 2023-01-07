45677dd9dfaa34de7a11c464a1859d460a557548
nikitafin <finogenov.nik@gmail.com>
2023-01-07 13:28:43 +0300

    start ods

---
+++ ods/ch1/ex1.1.cc
@@ -0,0 +1,6 @@
+/// @file ex1.1.cc
+///
+/// Read the input one line at a time
+/// and then write the lines out in reverse order,
+/// so that the last input line is printed first,
+/// then the second last input line, and so on.
