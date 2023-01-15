4b16eca1d476a6e7ee191c43597a7960e294a957
nikitafin <finogenov.nik@gmail.com>
2023-01-15 14:41:20 +0300

    some project stucture refs

--- ods/ch1/ex1.1.cc
+++ ods/ch1/ex1.1.cc
@@ -1,6 +1,10 @@
 /// @file ex1.1.cc
-///
+
 /// Read the input one line at a time
 /// and then write the lines out in reverse order,
 /// so that the last input line is printed first,
 /// then the second last input line, and so on.
+
+int main()
+{
+}
